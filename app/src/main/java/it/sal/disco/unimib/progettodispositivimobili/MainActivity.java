8ac46dcdbb7fcf7bdc3d03aa563022a310619c71
GolubOleksandra <102955072+GolubOleksandra@users.noreply.github.com>
2023-11-03 01:29:39 +0100

    risolto alcuni errori di configurazione, aggiunto il logo, aggiunto scrollview per migliore ridimensionamento, serve ancora sistemare login via google

--- app/src/main/java/it/sal/disco/unimib/progettodispositivimobili/MainActivity.java
+++ app/src/main/java/it/sal/disco/unimib/progettodispositivimobili/MainActivity.java
@@ -1,9 +1,18 @@
 package it.sal.disco.unimib.progettodispositivimobili;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,20 +20,50 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 import it.sal.disco.unimib.progettodispositivimobili.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    TextView textView;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    MaterialToolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.iconLogout) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
         View view = binding.getRoot();
         setContentView(view);
+
+        textView = findViewById(R.id.user_details);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+        toolbar = findViewById(R.id.top_appbar);
+        setSupportActionBar(toolbar);
+
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
                 findFragmentById(R.id.nav_host_fragment_activity_main);
@@ -38,6 +77,17 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        if(user == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+
+
+
+    }
 }
