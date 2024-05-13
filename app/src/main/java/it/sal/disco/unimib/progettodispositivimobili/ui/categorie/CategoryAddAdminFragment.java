048b2b89d3c17f81890bcc8dec6e421778fe28be
GolubOleksandra <102955072+GolubOleksandra@users.noreply.github.com>
2024-05-13 19:45:08 +0200

    aggiunta la parte per aggiunta/eliminazione/ricerca delle categorie dei comics da parte di admin + ancora da gestire il caricamento delle immagini degli utenti

--- app/src/main/java/it/sal/disco/unimib/progettodispositivimobili/ui/categorie/CategoryAddAdminFragment.java
+++ app/src/main/java/it/sal/disco/unimib/progettodispositivimobili/ui/categorie/CategoryAddAdminFragment.java
@@ -1,21 +1,41 @@
 package it.sal.disco.unimib.progettodispositivimobili.ui.categorie;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+
 import it.sal.disco.unimib.progettodispositivimobili.R;
 import it.sal.disco.unimib.progettodispositivimobili.databinding.FragmentCategoryAddAdminBinding;
 
 public class CategoryAddAdminFragment extends Fragment {
 
-    FragmentCategoryAddAdminBinding binding;
+    private FragmentCategoryAddAdminBinding binding;
+
+    private FirebaseAuth firebaseAuth;
+
+    private ArrayList<ModelCategory> categoryArrayList;
+
+    private AdapterCategory adapterCategory;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,6 +43,29 @@ public class CategoryAddAdminFragment extends Fragment {
 
         binding = FragmentCategoryAddAdminBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loadCategories();
+
+        binding.searchCategory.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    adapterCategory.getFilter().filter(s);
+                }catch (Exception e){}
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         binding.addCategoryBtn.setOnClickListener(v -> {
             if(getActivity() != null) {
@@ -32,6 +75,35 @@ public class CategoryAddAdminFragment extends Fragment {
 
 
         return root;
+    }
+
+    private void loadCategories() {
+        categoryArrayList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (binding == null) {
+                    return; // Exit if the view is destroyed
+                }
+                categoryArrayList.clear();
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+                    if (model != null) {
+                        categoryArrayList.add(model);
+                    }
+                }
+                adapterCategory = new AdapterCategory(getActivity(), categoryArrayList);
+
+                binding.RecyclerViewCategory.setAdapter(adapterCategory);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void openFragment(Fragment fragment){
